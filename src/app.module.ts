bd32c53f789e3d6d47534ccd8c0a279c807f2303
Luis <blackkndd@gmail.com>
2023-05-09 21:57:14 -0500

    haciendo commit a la conección con la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      process.env.MONGODB || 'mongodb://localhost:27017/nest-pokemon',
-    ),
+    // MongooseModule.forRoot(
+    //   process.env.MONGODB || 'mongodb://localhost:27017/nest-pokemon',
+    // ),
     PokemonModule,
   ],
   controllers: [],
