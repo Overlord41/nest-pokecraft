14f1efecab573ac601b2c89ad5dac16d2bd46437
Luis <blackkndd@gmail.com>
2023-05-11 12:43:43 -0500

    descomentando variables de entorno y coneccion db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   isGlobal: true,
-    // }),
-    // MongooseModule.forRoot(process.env.MONGODB),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
   ],
   controllers: [],
