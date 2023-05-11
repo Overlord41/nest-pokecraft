fef97a6cb7c3489fdba383785e17f1614cecb58d
Luis <blackkndd@gmail.com>
2023-05-11 02:01:07 -0500

    agregando variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://pokemondb_user:mj0tzEiGprvS3lUm@pokemoncluster.hmtu2dh.mongodb.net/nest-pokemon',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
   ],
   controllers: [],
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,7 +5,8 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 @Injectable()
 export class PokemonService {
   create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+    // return 'This action adds a new pokemon';
+    return `Esta es la conección a la db ${process.env.MONGODB}`;
   }
 
   findAll() {
