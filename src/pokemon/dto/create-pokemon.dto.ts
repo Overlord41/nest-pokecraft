80329f87f668915a498a0a6730a90b7d88450286
Luis <blackkndd@gmail.com>
2023-06-14 11:47:30 -0500

    agregando entity pokemon y validaciones al crear

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,41 +1,79 @@
+import { Type } from 'class-transformer';
 import {
   IsArray,
   IsBoolean,
   IsInt,
-  IsNumber,
   IsOptional,
   IsPositive,
   IsString,
   IsUrl,
   Min,
   MinLength,
+  ValidateNested,
 } from 'class-validator';
-import {
-  EvolutionInterface,
-  PokemonFilesInterface,
-} from '../interfaces/PokemonModel.interface';
 
-export class CreatePokemonDto {
-  @IsString()
-  @MinLength(3)
-  name: string;
-
+class EvolutionDto {
   @IsInt()
   @IsPositive()
   @Min(1)
   no: number;
 
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsUrl()
+  image: string;
+}
+
+class FilesPokemonDto {
   @IsUrl()
   image: string;
 
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  pages: number;
+
+  @IsBoolean()
+  pdo: boolean;
+
   @IsArray()
+  @IsString({ each: true })
+  links: string[];
+}
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsUrl()
+  image: string;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  generation: number;
+
+  @IsArray()
+  @IsString({ each: true })
   types: string[];
 
-  @IsArray()
   @IsOptional()
-  evolutions?: EvolutionInterface[];
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => EvolutionDto)
+  evolutions?: EvolutionDto[];
 
-  @IsArray()
   @IsOptional()
-  files?: PokemonFilesInterface[];
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => FilesPokemonDto)
+  files?: FilesPokemonDto[];
 }
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,18 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import {
-  EvolutionInterface,
-  PokemonFilesInterface,
-} from '../interfaces/PokemonModel.interface';
+
+class EvolutionPokemon {
+  @Prop({ required: true })
+  no: number;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  image: string;
+}
+
+class FilesPokemon {
+  @Prop({ required: true })
+  image: string;
+
+  @Prop({ required: true, default: 0 })
+  pages: number;
+
+  @Prop({ required: true, default: false })
+  pdo: boolean;
+
+  @Prop({ type: [String], required: true })
+  links: string[];
+}
 
 @Schema()
 export class Pokemon extends Document {
-  @Prop({
-    unique: true,
-    index: true,
-  })
-  name: string;
-
   @Prop({
     unique: true,
     index: true,
@@ -23,11 +38,24 @@ export class Pokemon extends Document {
     unique: true,
     index: true,
   })
+  name: string;
+
+  @Prop({
+    unique: true,
+  })
   image: string;
 
+  @Prop({ required: true })
+  generation: number;
+
+  @Prop({ type: [String], required: true })
   types: string[];
-  evolutions?: EvolutionInterface[];
-  files?: PokemonFilesInterface[];
+
+  @Prop({ required: false })
+  evolutions: EvolutionPokemon[];
+
+  @Prop({ required: false })
+  files: FilesPokemon[];
 }
 
 export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
