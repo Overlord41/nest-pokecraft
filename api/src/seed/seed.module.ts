a9e0dc2fd8cbff35514905966633d25e21c9c18d
Luis <blackkndd@gmail.com>
2023-10-09 11:17:45 -0500

    revisando todas las importaciones

--- api/src/seed/seed.module.ts
+++ api/src/seed/seed.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { PokemonModule } from '../pokemon/pokemon.module';
 import { CommonModule } from '../common/common.module';
 
 @Module({
